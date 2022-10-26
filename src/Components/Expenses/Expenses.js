e661139853d20718340b1cf7253ef0dc55e89c98
Yhozzim Dublan <emanueld.fbye@hotmail.com>
2022-10-26 17:52:36 -0500

    Final Commit

---
+++ src/Components/Expenses/Expenses.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
+
+const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) =>{
+      setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.items.filter(expense =>{
+      return expense.date.getFullYear().toString() === filteredYear; 
+    });
+
+    let expensesContent = <p>No expenses found.</p>;
+
+    if(filteredExpenses.length>0){
+      expensesContent = filteredExpenses.map((expense) => (
+        <ExpenseItem 
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount} 
+          date={expense.date}/>
+          ));
+        }
+
+    return(
+  <div>
+    <div className="expenses"> 
+        <ExpensesFilter 
+          selected={filteredYear} 
+          onChangeFilter = {filterChangeHandler}/>
+        {expensesContent}
+        <ExpensesChart expenses={filteredExpenses}/>
+    </div>
+  </div>
+    );
+};
+
+export default Expenses; 
